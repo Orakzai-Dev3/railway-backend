64a9fd5ff5ddda8f84f22d6fefa181a8088dc80e
Orakzai-Dev3 <arehman.dev3@gmail.com>
2025-05-02 03:47:34 +0500

    changed

--- controllers/controller_api.js
+++ controllers/controller_api.js
@@ -194,7 +194,7 @@ const Login = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            sameSite: 'none',
             maxAge: 3600000,
             path: '/',
         });
